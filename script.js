cd167d703adb67a1ca179b76f3b656e163b1e395
Andrés Devia <43211173+adeviat@users.noreply.github.com>
2025-10-29 16:08:08 +0100

    Inclusión de botones y CTA´s

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const letters = document.querySelectorAll('.fade-letter');
+const mensWearContainer = document.getElementById('mens-wear-container');
+const collectionBtn = document.getElementById('collection-btn');
+const scrollIndicator = document.getElementById('scroll-indicator');
 const maxScroll = window.innerHeight;
 let scrollY = 0;
-let isAnimating = false;
 
 window.addEventListener('wheel', e => {
   e.preventDefault();
 
-  // Actualizar scrollY según la rueda (deltaY)
   scrollY += e.deltaY;
-  scrollY = Math.max(0, Math.min(scrollY, maxScroll)); 
+  scrollY = Math.max(0, Math.min(scrollY, maxScroll));
   updateOpacity(scrollY);
+
+  // Indicador de hacer scroll
+  if (scrollY === 0) {
+    scrollIndicator.classList.remove('hidden');
+  } else {
+    scrollIndicator.classList.add('hidden');
+  }
+
+  if (scrollY >= maxScroll) {
+    mensWearContainer.classList.remove('hidden');
+    collectionBtn.classList.remove('hidden');
+    collectionBtn.classList.add('visible');
+  } else {
+    mensWearContainer.classList.add('hidden');
+    collectionBtn.classList.remove('visible');
+    collectionBtn.classList.add('hidden');
+  }
 }, { passive: false });
 
 function updateOpacity(scrollPos) {
@@ -23,6 +41,7 @@ function updateOpacity(scrollPos) {
 
     if (progress >= end) opacity = 1;
     else if (progress > start) opacity = (progress - start) * lettersCount;
+
     letter.style.opacity = opacity;
   });
 }
